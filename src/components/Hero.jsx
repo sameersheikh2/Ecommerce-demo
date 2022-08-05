44cacf809e075dc05f3a31fe5780276e9523ad07
sameersheikh2 <sameersheikh819@gmail.com>
2022-08-05 16:15:15 +0530

    made some style changes in hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = () => {
             {/* overlay */}
             <div className='flex flex-col flex-wrap items-center justify-center absolute  ease w-full h-full '>
                 <Link to="/products">
-                <button className='tracking-wide p-2 hover:tracking-widest hover:bg-black hover:text-white bg-white/40 cursor-pointer hover:scale-105 hover:border-white duration-100 text-6xl font-medium border-black border-[6px]'>SHOP</button>
+                <button className='tracking-wide p-2 hover:tracking-widest bg-white/40 cursor-pointer hover:scale-105  duration-100 text-6xl font-medium border-black border-[6px]'>SHOP</button>
                 </Link>
             </div>
-            <img src="https://images.unsplash.com/photo-1595665593673-bf1ad72905c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=original&q=80" alt="" className='object-cover w-full max-h-[600px]' />
+            <img src="https://images.unsplash.com/photo-1595665593673-bf1ad72905c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=original&q=80" alt="" className='object-cover rounded w-full max-h-[600px]' />
         </div>
     )
 }
