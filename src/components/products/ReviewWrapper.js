88632a9e7434f38047a623081b8cfdd098d0d365
sameersheikh2 <sameersheikh819@gmail.com>
2022-08-28 15:50:33 +0530

    render review componenets inside reviewWrapper

--- src/components/products/ReviewWrapper.js
+++ src/components/products/ReviewWrapper.js
@@ -1,10 +1,22 @@
-import React from "react";
-import Reviews from "./Reviews";
+import React, { useState } from "react";
+import ReviewFrom from "./ReviewForm";
+import Review from "./Review";
 
 const ReviewWrapper = () => {
+  const [reviews, setReviews] = useState([]);
+
+  const addReviewHandler = (reviews) => {
+    setReviews((preReviews) => {
+      return [...preReviews, reviews];
+    });
+  };
+
   return (
     <div id="review" className="w-[100%]">
-      <Reviews />
+      <ReviewFrom onAdd={addReviewHandler} />
+      <div className="w-[96%] mx-auto">
+        <Review reviews={reviews} />
+      </div>
     </div>
   );
 };
