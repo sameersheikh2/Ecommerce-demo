bd617b1c0cb47f6788362b656faaebd553e277f7
sameersheikh2 <sameersheikh819@gmail.com>
2022-08-29 21:22:56 +0530

    add api support for product category

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -31,37 +31,41 @@ const Products = () => {
     fetchData();
   }, []);
 
-  const productFilterHandler = (filterValue) => {
+  const productFilterHandler = async (filterValue) => {
     setSelectedFilter(filterValue);
-    setShowFilterOption(false);
+    try {
+      const res = await fetch(
+        `https://fakestoreapi.com/products/category/${filterValue}`
+      );
+      const data = await res.json();
+      setFilterProduct(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
     if (selectedFilter !== "") {
-      setFilterProduct(
-        products.filter((item) => {
-          return item.category === selectedFilter;
-        })
-      );
+      setFilterProduct(filterProduct);
     } else {
       setFilterProduct(products);
     }
-  }, [selectedFilter, products]);
+  }, [selectedFilter, products, filterProduct]);
 
   return (
     <>
-      {" "}
-      <div className="w-[95%] mx-auto scroll-smooth">
-        <h1 className="mt-12 ml-7 text-2xl font-medium">All Products -</h1>
+      <div className="w-[100%] mx-auto scroll-smooth">
+        <h1 className="mt-1 ml-7 text-2xl font-medium">All Products -</h1>
         <button
-          className="mt-4 ml-7 bg-white/80 text-base font-medium flex items-center rounded-lg shadow-3xl px-3 "
+          className="mt-3 ml-7 bg-white/80 text-base font-medium flex items-center rounded-lg shadow-3xl px-3 "
           onClick={() => {
             setShowFilterOption(!showFilterOption);
           }}
         >
           <TuneIcon style={{ fontSize: "17px", marginTop: "0px" }} /> Filter
         </button>
-        <div className="flex mt-3 items-start w-[95%] m-auto flex-col justify-start">
+
+        <div className="flex mt-3 mb-3 items-start w-[95%] m-auto flex-col justify-start">
           <button
             className="bg-white/80 rounded-lg shadow-4xl px-[6px] font-semibold"
             onClick={() => {
@@ -75,7 +79,7 @@ const Products = () => {
           </button>
           {showFilterOption && <Filter onFilter={productFilterHandler} />}
         </div>
-        <div className="flex flex-wrap gap-4 rounded justify-center">
+        <div className="flex flex-wrap justify-center">
           {filterProduct.map((product) => (
             <ProductList product={product} key={product.id} />
           ))}
