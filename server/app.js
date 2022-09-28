2b059fb930b5f0252ff8b9f7e9d6fa284c7525f2
sameersheikh2 <sameersheikh819@gmail.com>
2022-09-28 20:43:17 +0530

    add suport of mongoDb but connection error

--- server/app.js
+++ server/app.js
@@ -3,45 +3,42 @@ const app = express();
 const products = require("./products.json");
 const cors = require("cors");
 const port = 9000;
+const mongoose = require("mongoose");
 app.use(cors());
+
+async function connect() {
+  try {
+    await mongoose.connect(
+      "mongodb://sameersheikh1:sheikh.mongodb@mystore.00cjup1.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("connected");
+  } catch (error) {
+    console.error(error);
+  }
+}
+connect();
 
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
-app.get("/api/products/:id", (req, res) => {
-  //(two arg.)(path, call back fnc.)
-<<<<<<< HEAD
 
-=======
-  if (req.params.id !== products.length) {
-    res.status(404).send("error");
-    return;
-  }
->>>>>>> 316625c1f52ebd09c16354a0cddd3c4c20d7d32b
+app.get("/api/products/:id", (req, res) => {
   res.json(
     products.filter((product) => {
       return product.id === parseInt(req.params.id);
     })
   );
 });
+
 app.get("/api/products/category/:productCategory", (req, res) => {
   res.json(
-<<<<<<< HEAD
     products.filter((product) => {
       return product.category === req.params.productCategory;
-=======
-    products.find((product) => {
-      return product.category === parseInt(req.params.productCategory);
->>>>>>> 316625c1f52ebd09c16354a0cddd3c4c20d7d32b
     })
   );
 });
 
 app.listen(port, () => {
-<<<<<<< HEAD
   // listen to  req.
-=======
-  // listen to req.
->>>>>>> 316625c1f52ebd09c16354a0cddd3c4c20d7d32b
   console.log(`Example app listening on port ${port}`);
 });
