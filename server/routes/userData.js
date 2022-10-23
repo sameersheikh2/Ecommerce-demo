3b05f7d83f3a608bd919e462efb476466e656be3
sameersheikh2 <sameersheikh819@gmail.com>
2022-10-23 20:33:21 +0530

    update url and schema for userData

--- server/routes/userData.js
+++ server/routes/userData.js
@@ -1,17 +1,23 @@
+const { RssFeed } = require("@mui/icons-material");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.post("/", async (req, res) => {
+router.post("/create", async (req, res) => {
   const alreadyExist = await User.findOne({ email: req.body.email });
   if (alreadyExist !== null) {
-    res.json({ message: "email already exist" });
+    res.status(400).json({ message: "email already exist" });
     return;
   }
   const userData = await User({
     userName: req.body.name,
     email: req.body.email,
     password: req.body.password,
+    wishlist: {
+      title: req.body.wishlist.title,
+      price: req.body.price,
+      description: req.body.description,
+    },
   });
   try {
     userData.save();
@@ -19,6 +25,26 @@ router.post("/", async (req, res) => {
   } catch (err) {
     res.send(err);
   }
+});
+
+router.get("/", async (req, res) => {
+  User.find()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ error: err });
+    });
+});
+
+router.post("/account/find", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user !== null) {
+    res.status(200).send(user);
+    return;
+  }
+  res.status(400).json({ message: "Email dosen't match any account" });
 });
 
 // router.get("/", (req, res) => {
