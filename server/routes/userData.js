5acb6c2a2f2589f1b4776e516b73fae0a3037ab0
sameersheikh2 <sameersheikh819@gmail.com>
2022-11-25 20:44:26 +0530

    update forget password functionality

--- server/routes/userData.js
+++ server/routes/userData.js
@@ -1,4 +1,3 @@
-const { RssFeed } = require("@mui/icons-material");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
@@ -6,14 +5,14 @@ const User = require("../models/User");
 router.post("/create", async (req, res) => {
   const alreadyExist = await User.findOne({ email: req.body.email });
   if (alreadyExist !== null) {
-    res.status(400).json({ message: "email already exist" });
+    res.status(400).json({ message: "email already register" });
     return;
   }
   const userData = await User({
     userName: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    wishlist: {},
+    // wishlist: req.body.wishlist,
   });
   try {
     userData.save();
@@ -34,17 +33,22 @@ router.get("/", async (req, res) => {
     });
 });
 
-router.post("/account/find", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+router.get("/account/find/:userEmail", async (req, res) => {
+  const user = await User.findOne({ email: req.params.userEmail });
   if (user !== null) {
     res.status(200).json(user);
     return;
   }
-  res.status(400).json({ message: "Email dosen't match any account" });
+  res.status(400).json({ message: "Email dosen't match any account!" });
 });
 
-// router.get("/", (req, res) => {
-//   res.send("success");
-// });
-
+router.patch("/account/updatePassword", async (req, res) => {
+  if (req.body.password.length < 6) {
+    return res.status(400).json({ message: "password less than 6 character" });
+  }
+  const user = await User.findOne({ id: req.body.id });
+  user.password = req.body.password;
+  user.save();
+  res.status(200).json({ message: "password updated" });
+});
 module.exports = router;
--- src/pages/ForgetPassword.js
+++ src/pages/ForgetPassword.js
@@ -1,62 +1,116 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ForgetPassword = () => {
-  const [email, setEmail] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [user, setUser] = useState([]);
+  const [accountNotFound, setAccountNotFound] = useState(false);
   const [accountFound, setAccountFound] = useState(false);
+  const navigate = useNavigate();
 
   const findAccountHandler = (e) => {
     e.preventDefault();
     fetch(`http://localhost:9000/api/users/account/find/${email}`)
       .then((res) => {
         if (!res.ok) {
-          setAccountFound(true);
+          setAccountNotFound(true);
           return res.json();
         }
-        console.log("running");
-        setAccountFound(false);
+        setAccountNotFound(false);
+        setAccountFound(true);
+        setEmail("");
         return res.json();
       })
       .then((data) => setUser(data))
       .catch((err) => console.log(err));
   };
-  console.log(user);
+
+  const updatePassword = (e) => {
+    e.preventDefault();
+    const userData = {
+      id: user._id,
+      email: email,
+      password: password,
+    };
+    fetch("http://localhost:9000/api/users/account/updatePassword", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => {
+        if (res.ok) {
+          navigate("/login");
+          return res.json();
+        }
+        return res.json();
+      })
+      .then((data) => alert(data.message));
+  };
 
   return (
     <>
-      <form
-        className="flex m-auto bg-white rounded shadow-4xl w-[40%] mt-8 flex-col p-7"
-        onSubmit={findAccountHandler}
-      >
-        <h1 className="mb-6 text-lg">Find your account</h1>
+      <form className="flex m-auto bg-white rounded shadow-4xl w-[50%] mt-8 flex-col p-7">
+        <h1 className="mb-6 text-lg font-medium">Forget password</h1>
+        {accountFound ? <h2 className="mb-4">{user.userName}</h2> : null}
         <label htmlFor="email">
           {!user.email ? "Enter your email" : "Enter new password"}
         </label>
-        <input
-          type="email"
-          required
-          className={
-            accountFound
-              ? "p-1 my-2 px-2 outline-none border border-red-500 rounded bg-red-200/60 focus:bg-gray-300 "
-              : "p-1 my-2 px-2 outline-none border-none rounded bg-gray-200/60 focus:bg-gray-300"
-          }
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        {accountFound && (
-          <span className="text-red-500">Email dosen't match any account!</span>
+        {accountFound ? (
+          <input
+            type="password"
+            required
+            value={password}
+            className={
+              accountNotFound
+                ? "p-1 my-2 px-2 outline-none w-[75%] border border-red-500 rounded bg-red-200/60 focus:bg-gray-300 "
+                : "p-1 my-2 px-2 outline-none w-[75%] border-none rounded bg-gray-200/60 focus:bg-gray-300"
+            }
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        ) : (
+          <input
+            type="email"
+            required
+            value={email}
+            className={
+              accountNotFound
+                ? "p-1 my-2 px-2 outline-none w-[75%] border border-red-500 rounded bg-red-200/60 focus:bg-gray-300 "
+                : "p-1 my-2 px-2 outline-none w-[75%] border-none rounded bg-gray-200/60 focus:bg-gray-300"
+            }
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        )}
+        {accountNotFound && (
+          <span className="text-red-500">{user.message}</span>
         )}
         <div className="flex justify-evenly">
-          <button className="w-[7rem] p-[2px] text-center rounded-3xl border border-gray-400 mt-4 hover:text-blue-500 hover:border-blue-500 text-sm">
-            <Link to="/login">Back to login</Link>
-          </button>
-          {
-            <button className="w-[7rem] p-[2px] text-center rounded-3xl border border-gray-400 mt-4 hover:text-blue-500 hover:border-blue-500 text-sm">
+          <Link
+            to="/login"
+            className="w-[7rem] p-[2px] text-center rounded-3xl border border-gray-400 mt-4 hover:text-blue-500 hover:border-blue-500 text-sm"
+          >
+            Back to login
+          </Link>
+
+          {accountFound ? (
+            <button
+              className="w-[7rem] p-[2px] text-center rounded-3xl border border-gray-400 mt-4 hover:text-blue-500 hover:border-blue-500 text-sm"
+              onClick={updatePassword}
+            >
+              Update
+            </button>
+          ) : (
+            <button
+              className="w-[7rem] p-[2px] text-center rounded-3xl border border-gray-400 mt-4 hover:text-blue-500 hover:border-blue-500 text-sm"
+              onClick={findAccountHandler}
+            >
               Next
             </button>
-          }
+          )}
         </div>
       </form>
     </>
